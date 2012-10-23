ca28b411452c254e0d4b962963d8b0b1fa40ad5c
Annabelle Buffart <anna.buffart@gmail.com>
2012-10-23 11:14:28 +0200

    style.css/jquery.js/javascript.js lié

--- application/views/layout.php
+++ application/views/layout.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Ajouter un lien</title>
-    <script type="text/javascript" src="<?php echo base_url('web/jquery.js') ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('web/javascript.js') ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('web/jquery.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('web/javascript.js'); ?>"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="<?php echo base_url('web/style.css') ?>"
+          href="<?php echo base_url('web/style.css'); ?>"
           media="screen"
           title="Normal"/>
 
---
+++ web/javascript.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: annabelle
+ * Date: 23/10/12
+ * Time: 11:03
+ * To change this template use File | Settings | File Templates.
+ */
